b719d76c846875e43ee8b41a9691e75ea577e53d
João Brönstrup <joaog.gob@gmail.com>
2023-11-23 21:48:24 -0300

    feat: funcionalidade login

--- api/index.js
+++ api/index.js
@@ -17,6 +17,20 @@ async function cadastraUsuario (email, senha) {
     catch {
         console.error("Deu erro!")
     }
-}
+} export default cadastraUsuario;
 
-export default cadastraUsuario;
+
+
+export async function buscaUsuario (emailLogin) {
+        const requisita = await fetch("http://localhost:8000/unico", {
+            method: "post",
+            body: JSON.stringify({
+                email: emailLogin
+            }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+        const resposta = await requisita.json();
+        return resposta;
+}
