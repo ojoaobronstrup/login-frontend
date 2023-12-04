6f319cf3c26f8a291be8d2befe6c7942af4cda59
João Brönstrup <joaog.gob@gmail.com>
2023-12-03 21:13:52 -0300

    fix: pagina inicial

--- src/pages/Inicio/index.jsx
+++ src/pages/Inicio/index.jsx
@@ -10,50 +10,17 @@ const Corpo = styled.div`
     flex-direction: column;
     gap: 15px;
 `
-const Horario = styled.h1`
-    font-size: 50px;
-    font-weight: bold;
-`
-const Botao = styled.button`
-    background-color: black;
-    color: white;
-    width: 200px;
-    height: 50px;
-    border-radius: 10px;
-    border: 0;
+const Img = styled.img`
     cursor: pointer;
 `
 
 function Inicio () {
-    const [segundos, setSegundos] = useState(new Date().getSeconds());
-    const [minutos, setMinutos] = useState(new Date().getMinutes());
-    const [horas, setHoras] = useState(new Date().getHours());
-    const [dia, setDia] = useState(new Date().getDate());
-    const [mes, setMes] = useState(new Date().getMonth());
-    const [ano, setAno] = useState(new Date().getFullYear());
-    
-    useEffect(() => {
-       setInterval(() => {
-            setSegundos(new Date().getSeconds());
-            setMinutos(new Date().getMinutes());
-            setHoras(new Date().getHours());
-        }, 1000);
-        setDia(new Date().getDate());
-        setMes(new Date().getMonth()+1);
-        setAno(new Date().getFullYear());
-    }, []);
-
     return (
         <>
             <Corpo>
-                <h2>{dia}/{mes}/{ano}</h2>
-                <Horario>{horas}:{minutos}:{segundos}</Horario>
-                <Botao onClick={()=> {
-                    return (
-                        alert("Ponto marcado com sucesso!"),
-                        setInterval(()=> {window.location.pathname = "/"}, 1000)
-                    )
-                }}>Marcar Ponto</Botao>
+                <Img src="/entrou.jpeg" alt="Logo de um certinho" onClick={()=>{
+                    return window.location.pathname = "/"
+                }}/>
             </Corpo>
         </>
     )
